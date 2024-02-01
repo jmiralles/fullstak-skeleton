c2a735526d074b67d15b98246c0beb1300a0df16
jmiralles <jmirallespaez@gmail.com>
2024-02-01 21:41:40 +0100

    add open api validation

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,19 @@
 import express from "express";
 import cors from "cors";
-import { todosRoute } from "./controllers/todos/todo.router";
+import { todoRoute } from "./controllers/todos/todo.router";
 import { sendErrorResponse } from "./error-handling/error-handler";
+import { serveOpenapiSpec } from "./pre-request-handlers/openapi";
 
 const app = express();
 
 app.use(cors());
+
 // Enable JSON body parsing
 app.use(express.json({ limit: "1mb" }));
+app.use("/openapi.json", serveOpenapiSpec);
 
 // Mount the `/todos` resource
-app.use("/todos", todosRoute);
+app.use("/todos", todoRoute);
 
 app.use(sendErrorResponse);
 
